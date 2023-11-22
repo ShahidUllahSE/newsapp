9e7814a5da62568eaccf7d0de21c1e030d86b20c
ShahidUllahSE <shahidbangash8899@gmail.com>
2023-11-22 11:35:18 +0500

    added more functionaity

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { useState } from 'react'
- import NavBar from './components/NavBar'; 
+import NavBar from './components/NavBar';
 import {
   BrowserRouter as Router,
   Route,
@@ -11,70 +11,68 @@ import LoadingBar from 'react-top-loading-bar'
 
 
 
-const App=()=> {
+const App = () => {
   // const [progress, setprogress] = useState(0)
 
-        return (
-          <Router> 
-            <div>
-           
-              <NavBar />
-              <Routes>
-                <Route
-                  path="/"
-                  element={<News key="general" pageSize={5} country="in" category="general" />}
-                ></Route>
+  return (
+    <Router>
+      <div>
 
-                  <Route
-                  path="/business"
-                  element={<News key="business" pageSize={5} country="in" category="business" />}
-                ></Route>
+        <NavBar />
+        <Routes>
+          <Route
+            path="/"
+            element={<News key="general" pageSize={5} country="in" category="general" />}
+          ></Route>
 
-                  <Route
-                  path="/entertainment"
-                  element={<News key="entertainment" pageSize={5} country="in" category="entertainment" />}
-                ></Route>
+          <Route
+            path="/business"
+            element={<News key="business" pageSize={5} country="in" category="business" />}
+          ></Route>
 
+          <Route
+            path="/entertainment"
+            element={<News key="entertainment" pageSize={5} country="in" category="entertainment" />}
+          >
+            
+          </Route>
 
+          <Route
+            path="/general"
+            element={<News key="general" pageSize={5} country="in" category="general" />}
+          ></Route>
 
-                  <Route
-                  path="/general"
-                  element={<News key="general" pageSize={5} country="in" category="general" />}
-                ></Route>
+          <Route
+            path="/health"
+            element={<News key="health" pageSize={5} country="in" category="health" />}
+          ></Route>
 
-
-
-                  <Route
-                  path="/health"
-                  element={<News key="health" pageSize={5} country="in" category="health" />}
-                ></Route>
-
-                <Route
-                  path="/science"
-                  element={<News key="science" pageSize={5} country="in" category="science" />}
-                ></Route>
+          <Route
+            path="/science"
+            element={<News key="science" pageSize={5} country="in" category="science" />}
+          ></Route>
 
 
-                <Route
-                  path="/sports"
-                  element={<News key="sports" pageSize={5} country="in" category="sports" />}
-                ></Route>
+          <Route
+            path="/sports"
+            element={<News key="sports" pageSize={5} country="in" category="sports" />}
+          ></Route>
 
 
 
-                  <Route
-                  path="/technology"
-                  element={<News key="technology" pageSize={5} country="in" category="technology" />}
-                ></Route>
+          <Route
+            path="/technology"
+            element={<News key="technology" pageSize={5} country="in" category="technology" />}
+          ></Route>
 
 
 
 
-              </Routes>
-            </div>
-          </Router>
-        );
-      };
-      
-      export default App;
-      
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
+
